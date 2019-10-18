da12a82f578947905a865c89397ccd242c56f68c
guanqunyin <1151900425@qq.com>
2019-10-18 12:20:15 +0800

    添加了三种破坏单例模式的方式。 1. 反射， 当调用私有构造函数的时候，直接抛出运行时一场 2. clone， 在重写的clone方法中直接调用getInstance方法 3. 序列化和反序列化， 在重写的readResolve方法中 直接调用getInstance方法

--- src/com/yin/pattern/singleton/Singleton5.java
+++ src/com/yin/pattern/singleton/Singleton5.java
@@ -1,12 +1,19 @@
 package com.yin.pattern.singleton;
 
+import java.io.*;
+
 /**
  * Created by Administrator on 2019/10/17.
  * 饿汉式双重锁
  */
-public class Singleton5 {
+public class Singleton5 implements Cloneable, Serializable{
 
-    private Singleton5(){}
+
+    private Singleton5(){
+        if (null != singleton5) {
+            throw new RuntimeException();
+        }
+    }
 
     private static volatile Singleton5 singleton5;
 
@@ -20,15 +27,39 @@ public class Singleton5 {
         return singleton5;
     }
 
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return singleton5;
+    }
+
+    public Object readResolve() {
+        return singleton5;
+    }
+
+
+
     public static void main(String[] args) throws Exception {
         //通过反射破坏单例模式
-        Object o = Singleton5.class.newInstance();
         System.out.println(Singleton5.getInstance());
+        Object o = Singleton5.class.newInstance();
         System.out.println(o);
 
-        //TODO 通过反射破坏单例模式
-        //TODO 通过clone破坏单例模式
+        //通过clone破坏单例模式, 解决方案，重写clone方法，直接返回单例对象
+        Singleton5 singleton5 = Singleton5.getInstance();
+        Object singleton5Cloned = singleton5.clone();
+        System.out.println(singleton5);
+        System.out.println(singleton5Cloned);
         //TODO 通过序列化和反序列化破坏单例模式
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try(ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("tempfile"))){
+            outputStream.writeObject(singleton5);
+            outputStream.flush();
+        }
+
+        try(ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("tempfile"))){
+            Object object = inputStream.readObject();
+            System.out.println(object);
+        }
     }
 
 }
