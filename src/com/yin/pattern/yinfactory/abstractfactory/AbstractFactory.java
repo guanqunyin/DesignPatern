855befd3bd139d5c1e649220148627494af3d9de
guanqunyin <1151900425@qq.com>
2019-10-22 12:46:34 +0800

    工厂方法：单独的一个产品维度， 无法对产品一族进行维护 抽象工厂： 对产品一族进行维护， 无法对单一的产品进行维护， spring bean工厂：通过更改配置文件来实现灵活的配置产品。

--- src/com/yin/pattern/yinfactory/abstractfactory/AbstractFactory.java
+++ src/com/yin/pattern/yinfactory/abstractfactory/AbstractFactory.java
@@ -1,5 +1,8 @@
 package com.yin.pattern.yinfactory.abstractfactory;
 
+/*
+    在产品一族进行灵活的扩展
+ */
 public abstract class AbstractFactory {
 
     abstract Vihicle createVihicle();
--- src/com/yin/pattern/yinfactory/factorymethod/CarFactory.java
+++ src/com/yin/pattern/yinfactory/factorymethod/CarFactory.java
@@ -2,6 +2,7 @@ package com.yin.pattern.yinfactory.factorymethod;
 
 /**
  * Created by Administrator on 2019/10/20.
+ * 在产品维度上进行扩展
  */
 public class CarFactory {
 
