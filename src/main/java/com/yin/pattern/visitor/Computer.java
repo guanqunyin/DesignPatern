9c5a848b28c5e2486f292692f6d7e631088037c3
guanqunyin <1151900425@qq.com>
2019-10-30 22:04:08 +0800

    访问者模式：在内部结构不变的情况下改变对于内部的访问。 通常比如有说运用在编译器的抽象语法树上面
    暂时还不是很理解这种方式，需要再去多看几遍理解一下

--- src/main/java/com/yin/pattern/visitor/Computer.java
+++ src/main/java/com/yin/pattern/visitor/Computer.java
@@ -2,7 +2,21 @@ package com.yin.pattern.visitor;
 
 public class Computer {
 
+    ComputerPart cpu = new CPU();
+    ComputerPart memory = new Memory();
+    ComputerPart board = new Board();
+
+    public void accept(Visitor visitor) {
+        this.cpu.accept(visitor);
+        this.memory.accept(visitor);
+        this.board.accept(visitor);
+    }
+
     public static void main(String[] args) {
+        Computer computer = new Computer();
+        StudentVisitor studentVisitor = new StudentVisitor();
+        computer.accept(studentVisitor);
+        System.out.println(studentVisitor.price);
 
     }
 
@@ -10,48 +24,76 @@ public class Computer {
 
 abstract class ComputerPart{
 
-    abstract void accept();
+    abstract void accept(Visitor visitor);
 
     abstract double getPrice();
-
 
 }
 
 class CPU extends ComputerPart {
 
     @Override
-    void accept() {
-
+    void accept(Visitor visitor) {
+        visitor.visitCPU(this);
     }
 
     @Override
     double getPrice() {
-        return 0;
+        return 2000;
     }
 }
 
 class Memory extends ComputerPart {
 
     @Override
-    void accept() {
-
+    void accept(Visitor visitor) {
+        visitor.visitMemory(this);
     }
 
     @Override
     double getPrice() {
-        return 0;
+        return 500;
     }
 }
 
 class Board extends ComputerPart {
 
     @Override
-    void accept() {
-
+    void accept(Visitor visitor) {
+        visitor.visitBoard(this);
     }
 
     @Override
     double getPrice() {
-        return 0;
+        return 1000;
+    }
+}
+
+interface Visitor{
+
+    void visitCPU(CPU cpu);
+
+    void visitBoard(Board board);
+
+    void visitMemory(Memory memory);
+}
+
+class StudentVisitor implements Visitor{
+
+    double price;
+
+    @Override
+    public void visitCPU(CPU cpu) {
+        price = price + cpu.getPrice()*0.6;
+    }
+
+    @Override
+    public void visitBoard(Board board) {
+        price = price + board.getPrice()*0.6;
+    }
+
+    @Override
+    public void visitMemory(Memory memory) {
+        price = price + memory.getPrice()*0.6;
     }
 }
